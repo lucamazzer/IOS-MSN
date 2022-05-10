6f9ab10b0c1ee1e6359befe3ff7bae7aea14f1f5
luca andrea mazzer <lucamazzer92@gmail.com>
2022-05-10 12:40:16 -0300

    init proyect

---
+++ Messenger/Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by luca andrea mazzer on 10/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
